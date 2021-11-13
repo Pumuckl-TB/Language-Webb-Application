6b7b61d5e03e99c70282f02496e592d4f89d7fdd
tatjanaferri <92537880+tatjanaferri@users.noreply.github.com>
2021-11-13 12:54:40 +0100

    copy experiment to add datastudio

---
+++ frontend/experimentation data_studio.py
@@ -0,0 +1,38 @@
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+    html.Div(className='row',  # Define the row element
+        children=[ # Define the left element
+            html.Div(className='three columns div-user-controls',
+                children = [
+                    html.H2('Teacher View',style={'color': 'white'}),
+                    html.P('''Select student to view''',style={'color': 'white'}),
+                ],
+                style={'backgroundColor': '#1E1E1E'}),
+            
+            html.Div(className='nine columns div-user-controls bg-grey', # Define the right element
+                children = [
+                    html.H2('Right Side Element',style={'color': 'white'}),
+                    html.P('''Bla Bla Bla''',style={'color': 'white'}),
+                    html.Img(src='https://raw.githubusercontent.com/feljost/Language-WebApp/main/frontend/pictures/Placeholder_studenttable.png',
+                        style={
+                            'height' : '50%',
+                            'width' : '50%'}
+                            ) 
+            ])
+        ])
+])
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True, use_reloader=False)
+    # app.run_server(host = '0.0.0.0', port = 8050, debug = True)
+
+
