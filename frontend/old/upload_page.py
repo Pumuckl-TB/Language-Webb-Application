f1ec6ade646b36b50ead30719d4358ff67f63b88
feljost <63246300+feljost@users.noreply.github.com>
2021-12-01 20:25:20 +0100

    ...

--- frontend/old/upload_page.py
+++ frontend/old/upload_page.py
@@ -48,7 +48,7 @@ app.layout = html.Div(style={'backgroundColor':'#FFFFFF'}, children=[
             dcc.Upload(
                 id='upload-data',
                 children=html.Div([
-                    html.P('Drag and Drop or '),
+                    html.P('Drag and Drop or ', style={'font-family':'arial'}),
                     html.A('Select Files')
                 ]),
                 style={
