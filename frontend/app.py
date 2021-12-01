8d2e722ea425c854b23883c6adfd3ed7460496ec
feljost <63246300+feljost@users.noreply.github.com>
2021-12-01 21:27:53 +0100

    favicon and tab renaming

--- frontend/app.py
+++ frontend/app.py
@@ -5,4 +5,6 @@ app = dash.Dash(__name__, suppress_callback_exceptions=True,
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
+app.title = 'Personal-German-Webiste'
+app._favicon = ("assets/favicon.ico")
 server = app.server
